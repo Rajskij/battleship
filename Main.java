c66960b48e7450ce3367c3be9e5476721b5d740e
Igor <68941665+Rajskij@users.noreply.github.com>
2020-11-24 18:36:49 +0200

    release v0.1

---
+++ Main.java
@@ -0,0 +1,78 @@
+package battleship;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] field = new String[10][10];
+        String[] ships = {"Aircraft Carrier (5 cells)", "Battleship (4 cells)", "Submarine (3 cells)", "Cruiser (3 cells)", "Destroyer (2 cells)"};
+        String nose, tail;
+        int row1 = 0, col1 = 0, row2 = 0, col2 = 0;
+        boolean correct = false;
+
+        makeField(field);
+        printField(field);
+
+        for (String s : ships) {
+            System.out.printf("Enter the coordinates of the %s:\n", s);
+            int lengthOfShip = Integer.parseInt(s.replaceAll("[^\\d]", ""));
+
+            while (!correct) {
+                nose = sc.next();
+                tail = sc.next();
+                row1 = stringToInt(nose);
+                col1 = Integer.parseInt(nose.substring(1));
+                row2 = stringToInt(tail);
+                col2 = Integer.parseInt(tail.substring(1));
+                ValidInput validInput = new ValidInput(field, row1, col1, row2, col2, lengthOfShip);
+                correct = validInput.checkIfAvailable() && validInput.checkLocation() && validInput.checkSheepLength(s);
+            }
+            correct = false;
+            fillField(field, row1, col1, row2, col2);
+            printField(field);
+        }
+    }
+
+    public static int stringToInt(String s) {
+        String val = "ABCDEFGHIJ";
+        return val.indexOf(s.charAt(0)) + 1;
+    }
+
+    public static String[][] makeField(String[][] str) {
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j < str[0].length; j++) {
+                str[i][j] = " ~";
+            }
+        }
+        return str;
+    }
+
+    public static String[][] fillField(String[][] str, int row1, int col1, int row2, int col2) {
+        for (int i = 0; i < str[0].length; i++) {
+            for (int j = 0; j < str.length; j++) {
+                if (j >= Math.min(col1, col2) - 1
+                        && j <= Math.max(col1, col2) - 1
+                        && i >= Math.min(row1, row2) - 1
+                        && i <= Math.max(row1, row2) - 1) {
+                    str[i][j] = " O";
+                }
+            }
+        }
+        return str;
+    }
+
+    public static void printField(String[][] str) {
+        char ch = 65;
+        System.out.println("  1 2 3 4 5 6 7 8 9 10");
+        for (String[] row : str) {
+            System.out.print(ch);
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+            ch++;
+        }
+    }
+}
---
+++ ValidInput.java
@@ -0,0 +1,61 @@
+package battleship;
+
+public class ValidInput {
+    private String[][] str;
+    private int row1;
+    private int col1;
+    private int row2;
+    private int col2;
+    private int lengthOfShip;
+
+    public ValidInput(String[][] str, int row1, int col1, int row2, int col2, int lengthOfShip) {
+        this.str = str;
+        this.row1 = row1;
+        this.col1 = col1;
+        this.row2 = row2;
+        this.col2 = col2;
+        this.lengthOfShip = lengthOfShip;
+    }
+    public boolean checkSheepLength(String s) {
+        boolean answer = true;
+        if (row1 == row2 && Math.max(col1,col2) - Math.min(col1,col2) != lengthOfShip - 1
+                || col1 == col2 && Math.max(row1,row2) - Math.min(row1,row2) != lengthOfShip - 1) {
+            System.out.printf("Error! Wrong length of the %s! Try again:\n", s.replaceAll(" \\(\\b.+", ""));
+            answer = false;
+        }
+        return answer;
+    }
+    public boolean checkLocation() {
+        boolean answer = true;
+        if (row1 != row2 && col1 != col2) {
+            System.out.println("Error! Wrong ship location! Try again:");
+            answer = false;
+        }
+        return answer;
+    }
+    public boolean checkIfAvailable() {
+        boolean answer = true;
+        for (int i = 0; i < str.length && answer; i++) {
+            for (int j = 0; j < str[0].length && answer; j++) {
+                if (answer && j >= Math.min(col1, col2) - 1 && j <= Math.max(col1, col2) - 1
+                        && i >= Math.min(row1, row2) - 1 && i <= Math.max(row1, row2) - 1) {
+
+                    int n = str.length;
+                    int down = i == n - 1 ? i : (i + 1) % n;    //if "i" or "j" is bigger then
+                    int up = i == 0 ? i : (i + n - 1) % n;      //array length, we left the count on the edge
+                    int right = j == n - 1 ? j : (j + 1) % n;
+                    int left = j == 0 ? j : (j + n - 1) % n;
+
+                    if (str[i][right].equals(" O") || str[i][left].equals(" O") //horizontal check
+                            || str[up][j].equals(" O") || str[down][j].equals(" O") //vertical check
+                            || str[down][right].equals(" O") || str[down][left].equals(" O") //diagonal top
+                            || str[up][right].equals(" O") || str[up][left].equals(" O") ) { //diagonal bottom
+                        answer = false;
+                        System.out.println("Error! You placed it too close to another one. Try again:");
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
